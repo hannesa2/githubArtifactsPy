eb43bb05126c7a2ba0f0e4f4b634cdf64cfdcce5
Hannes Achleitner <hannes.software@gmx.at>
2024-01-13 10:00:41 +0100

    File handling

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 from flask import Flask, render_template, request, redirect
+import os
 
 app = Flask(__name__, template_folder='templates')
 
 items = []  # This list will store the items
+UPLOAD_FOLDER = "files"
 
 
 @app.route('/')
 def index():
+    items = os.listdir(UPLOAD_FOLDER)
     return render_template('index.html', items=items)
 
+
+@app.route('/get', methods=['GET'])
+def get():
+    name = request.args['name']
+    try:
+        fp = open(UPLOAD_FOLDER + '/' + name, 'r')
+        content = fp.read()
+        fp.close()
+        return content
+    except FileNotFoundError:
+        print("Please check the path")
 
 @app.route('/create', methods=['POST'])
 def create():
     name = request.form['name']
-    items.append(name)
+
+    fp = open(UPLOAD_FOLDER + '/' + name, 'w')
+    fp.write('content of ' + name)
+    fp.close()
     return redirect('/')
 
 
@@ -21,19 +38,19 @@ def create():
 def update():
     old_name = request.form['old_name']
     new_name = request.form['new_name']
-    if old_name in items:
-        index = items.index(old_name)
-        items[index] = new_name
+    os.rename(UPLOAD_FOLDER + '/' + old_name, UPLOAD_FOLDER + '/' + new_name)
     return redirect('/')
 
 
 @app.route('/delete', methods=['POST'])
 def delete():
     name = request.form['name']
-    if name in items:
-        items.remove(name)
+    os.remove(UPLOAD_FOLDER + '/' + name)
     return redirect('/')
 
 
 if __name__ == '__main__':
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    items = os.listdir(UPLOAD_FOLDER)
     app.run(debug=True)
