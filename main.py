4c97e77911fc0f8c8b87865193cc21dd3765759f
Hannes Achleitner <hannes.software@gmx.at>
2024-01-14 11:46:50 +0100

    Authenticate

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-from flask import Flask, render_template, request, redirect, flash, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, flash, url_for, send_from_directory, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
 import os
 
 from werkzeug.utils import secure_filename
+from passlib.apps import custom_app_context as pwd_context
 
 app = Flask(__name__, template_folder='templates')
 
 UPLOAD_FOLDER = "files"
 ALLOWED_EXTENSIONS = {'txt', 'png', 'jpg', 'jpeg', 'gif'}
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+
+@app.route('/api/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)  # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)  # existing user
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'username': user.username}), 201, {'Location': url_for('get_user', id=user.id, _external=True)}
 
 
 @app.route('/')
@@ -91,5 +124,8 @@ if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.config['MAX_CONTENT_LENGTH'] = 200024
+    app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
     items = os.listdir(UPLOAD_FOLDER)
     app.run(debug=True)
